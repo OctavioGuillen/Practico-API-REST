c7c0e478b9b0bc856f2af70910e1b1b4a7c4d874
OctavioGuillen <octavioguillencardozo@gmail.com>
2024-09-21 23:47:11 -0300

    Add files via upload

---
+++ src/main/java/com/example/inicial1/entities/Persona.java
@@ -0,0 +1,30 @@
+package com.example.inicial1.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Audited
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "dni")
+    private int dni;
+
+
+}
+
